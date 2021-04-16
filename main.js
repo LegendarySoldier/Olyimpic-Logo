619c709b6745916947dc422bc505d1bc7f49fccf
Legendary Soldier <76736800+LegendarySoldier@users.noreply.github.com>
2021-04-16 14:30:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d"); 
+
+ctx.beginPath();
+ctx.strokeStyle = "grey";
+ctx.lineWidth = 1;
+ctx.rect(150, 143, 430, 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(250, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.arc(345, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arc(440, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(392.5, 250, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "#fff100";
+ctx.lineWidth = 5;
+ctx.arc(297.5, 250, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+
